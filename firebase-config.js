a820353f870160b9233ae35e2a750826a4091677
Qwerty-ally <finnshapiro32315@gmail.com>
2026-06-28 17:57:46 -0400

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAaOnohzImD2tJK6-BjcATNJEDV5fYtfOw",
+  authDomain: "anchor-af6e1.firebaseapp.com",
+  databaseURL: "https://anchor-af6e1-default-rtdb.firebaseio.com",
+  projectId: "anchor-af6e1",
+  storageBucket: "anchor-af6e1.firebasestorage.app",
+  messagingSenderId: "89138222333",
+  appId: "1:89138222333:web:89965004f82475dd685a94",
+  measurementId: "G-38ERWY3N3P"
+};
+
+const OWNER_USERNAME = "admin";
